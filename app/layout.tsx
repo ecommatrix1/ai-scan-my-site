043005e23eb9f3f6c00956cfeadf2487209d7720
ecommatrix1 <ecommatrixofficial@gmail.com>
2026-09-21 22:24:54 +0530

    fix(analytics): update Measurement ID to G-G60XQGT29T from live Analytics stream

--- app/layout.tsx
+++ app/layout.tsx
@@ -224,13 +224,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9283169427062874"
           crossOrigin="anonymous"
         />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5BLCDSQT31" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-G60XQGT29T" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
+              gtag('config', 'G-G60XQGT29T');
               gtag('config', 'G-5BLCDSQT31');
             `,
           }}

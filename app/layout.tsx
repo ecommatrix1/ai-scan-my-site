6fd137a2d504bf5e528483762315bcc8329c3449
ecommatrix1 <ecommatrixofficial@gmail.com>
2026-09-22 16:34:04 +0530

    fix(analytics): correct GA4 measurement ID typo to G-5BLCDSQT31

--- app/layout.tsx
+++ app/layout.tsx
@@ -224,14 +224,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9283169427062874"
           crossOrigin="anonymous"
         />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-GBLCD5QT31" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-5BLCDSQT31" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-GBLCD5QT31');
+              gtag('config', 'G-5BLCDSQT31');
             `,
           }}
         />
